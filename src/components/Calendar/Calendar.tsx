637f9cffbd2a47cb4787e5b1124f86b76a40ff74
bryantjandra <tjandra.benedict@gmail.com>
2026-04-05 15:14:45 +0800

    feat: add capability of adding todos

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,11 +1,49 @@
 import { DatePicker } from "@douyinfe/semi-ui";
 import styles from "./Calendar.module.css";
+import { useState } from "react";
 
 export default function Calendar() {
+  const [currDate, setCurrDate] = useState("");
+  const [todos, setTodos] = useState<string[]>([]);
+  const [inputText, setInputText] = useState("");
+
+  function handleInputChange(e) {
+    setInputText(e.target.value);
+  }
+
   return (
-    <DatePicker
-      className={styles.calendar}
-      placeholder={"hey phinneas, whatcha dooooin"}
-    />
+    <>
+      <DatePicker
+        className={styles.calendar}
+        placeholder={"hey phinneas, whatcha dooooin"}
+        onChange={(date, dateString) => {
+          setCurrDate(dateString as string);
+        }}
+      />
+      {currDate && (
+        <div className={styles.cardTodoList}>
+          <span className={styles.title}>Today's date is: {currDate} </span>
+          <input
+            value={inputText}
+            className={styles.inputTodo}
+            placeholder="enter todos."
+            onChange={(e) => {
+              handleInputChange(e);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setTodos([...todos, inputText]);
+                setInputText("");
+              }
+            }}
+          />
+          <ul className={styles.todoList}>
+            {todos.map((todo, index) => {
+              return <li key={index}>{todo}</li>;
+            })}
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
